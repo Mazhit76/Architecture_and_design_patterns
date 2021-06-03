05b5a4319d2f24ba489a08916ab4ec4630d8643c
Mazhit76 <62520216+Mazhit76@users.noreply.github.com>
2021-06-03 16:32:59 +0400

    Lession_7 done

--- patterns/architecture_patterns_mappers.py
+++ patterns/architecture_patterns_mappers.py
@@ -10,50 +10,50 @@ class StudentMapper:
         self.cursor = connection.cursor()
         self.tablename = 'student'
 
-def all(self):
-    statement = f'SELECT * from {self.tablename}'
-    self.cursor.execute(statement)
-    result = []
-    for item in self.cursor.fetchall():
-        id, name = item
-        student = Student(name)
-        student.id = id
-        result.append(student)
-    return result
+    def all(self):
+        statement = f'SELECT * from {self.tablename}'
+        self.cursor.execute(statement)
+        result = []
+        for item in self.cursor.fetchall():
+            id, name = item
+            student = Student(name)
+            student.id = id
+            result.append(student)
+        return result
 
-def find_by_id(self, id):
-    statement = f"SELECT id, name FROM {self.tablename} WHERE id=?"
-    self.cursor.execute(statement, (id,))
-    result = self.cursor.fetchone()
-    if result:
-        return Student(*result)
-    else:
-        raise RecordNotFoundException(f'record with id={id} not found')
+    def find_by_id(self, id):
+        statement = f"SELECT id, name FROM {self.tablename} WHERE id=?"
+        self.cursor.execute(statement, (id,))
+        result = self.cursor.fetchone()
+        if result:
+            return Student(*result)
+        else:
+            raise RecordNotFoundException(f'record with id={id} not found')
 
-def insert(self, obj):
-    statement = f"INSERT INTO {self.tablename} (name) VALUES (?)"
-    self.cursor.execute(statement, (obj.name,))
-    try:
-        self.connection.commit()
-    except Exception as e:
-        raise DbCommitException(e.args)
+    def insert(self, obj):
+        statement = f"INSERT INTO {self.tablename} (name) VALUES (?)"
+        self.cursor.execute(statement, (obj.name,))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbCommitException(e.args)
 
-def update(self, obj):
-    statement = f"UPDATE {self.tablename} SET name=? WHERE id=?"
-    # Где взять obj.id? Добавить в DomainModel? Или добавить когда берем объект из базы
-    self.cursor.execute(statement, (obj.name, obj.id))
-    try:
-        self.connection.commit()
-    except Exception as e:
-        raise DbUpdateException(e.args)
+    def update(self, obj):
+        statement = f"UPDATE {self.tablename} SET name=? WHERE id=?"
+        # Где взять obj.id? Добавить в DomainModel? Или добавить когда берем объект из базы
+        self.cursor.execute(statement, (obj.name, obj.id))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbUpdateException(e.args)
 
-def delete(self, obj):
-    statement = f"DELETE FROM {self.tablename} WHERE id=?"
-    self.cursor.execute(statement, (obj.id,))
-    try:
-        self.connection.commit()
-    except Exception as e:
-        raise DbDeleteException(e.args)
+    def delete(self, obj):
+        statement = f"DELETE FROM {self.tablename} WHERE id=?"
+        self.cursor.execute(statement, (obj.id,))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbDeleteException(e.args)
 
 
 
